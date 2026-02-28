89bc0b1fc8196d7453085a177dc0d062dc6e5a3f
Arcturus Emrys <aemrys@arcturus.live>
2026-02-28 16:24:24 +0000

    cargo fmt

--- src/io_adapter.rs
+++ src/io_adapter.rs
@@ -1,22 +1,28 @@
 use gio;
 use gio::prelude::*;
-use std::io::{Read, Error, ErrorKind};
+use std::io::{Error, ErrorKind, Read};
 
 /// Adaptor for using GIO files as regular Rust streams.
-/// 
+///
 /// I'm genuinely surprised gtk-rs does not ship with these.
 pub struct FileIn<T>(T);
 
-impl<T> From<T> for FileIn<T> where T: InputStreamExt {
+impl<T> From<T> for FileIn<T>
+where
+    T: InputStreamExt,
+{
     fn from(fis: T) -> Self {
         Self(fis)
     }
 }
 
-impl<T> Read for FileIn<T> where T: InputStreamExt {
+impl<T> Read for FileIn<T>
+where
+    T: InputStreamExt,
+{
     fn read(&mut self, buf: &mut [u8]) -> Result<usize, Error> {
-        self.0.read(buf, None::<&gio::Cancellable>).map_err(|gerror| {
-            Error::new(ErrorKind::Other, gerror)
-        })
+        self.0
+            .read(buf, None::<&gio::Cancellable>)
+            .map_err(|gerror| Error::new(ErrorKind::Other, gerror))
     }
 }
